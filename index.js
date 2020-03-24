088bc06862f39387a6377f22f24af02fbfee335c
John Loberger <johnloberger@gmail.com>
2020-03-24 23:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ function addToCart(item) {
 function viewCart() {
   return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+  }
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+  }
+  return `${cartDescription}.`
+}
 
 function total() {
   // write your code here
