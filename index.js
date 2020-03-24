27ed6f41d8ca39929e73b36dd827f43a6530fe97
John Loberger <johnloberger@gmail.com>
2020-03-24 23:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ function removeFromCart(item) {
     }
   })
   return cart
-} else {
-    return "That item is not in your cart.";
+}
   }
+  else {
+      return "That item is not in your cart."
+    }
 
 
 function placeOrder(cardNumber) {
