b793f298d2f2ce7e82ee5ce61a4e12af45e17a74
John Loberger <johnloberger@gmail.com>
2020-03-24 22:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-var price = Math.floor(Math.random()*99)
-cart.push(new Object({[item]:price}))
-console.log(`${item} has been added to your cart.`)
-return cart
+  const price = Math.floor(Math.random() * 100);
+  cart.push({ [item]: price});
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
+
 function viewCart() {
   // write your code here
 }
