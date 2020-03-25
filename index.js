55a195ca3bb5a95ac592e062958a868fbe37f1cf
John Loberger <johnloberger@gmail.com>
2020-03-25 00:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,18 +43,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  let itemInCart = false
-  for(let i = 0, l = cart.length; i < l; i++){
-    if(cart[i].hasOwnProperty(item)){
-      let index = cart.lastIndexOf(item)
-      cart.splice(index, 1)
-      itemInCart = true
-      return cart;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(removeitem)) {
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
     }
   }
-  if(itemInCart === false){
     console.log("That item is not in your cart.")
-  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
