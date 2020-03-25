c15cb9d8cf5276625323f5b8e73f8436c26dbe67
John Loberger <johnloberger@gmail.com>
2020-03-25 00:11:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,5 +62,12 @@ function notifyUserThereIsNoItemToRemove() {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments[0] == undefined) {
+      return "Sorry, we don't have a credit card on file for you."
+    } else {
+      var sumToCharge = total()
+      setCart([])
+      return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
+    }
+  }
 }
